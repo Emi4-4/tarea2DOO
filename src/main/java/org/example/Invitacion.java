d54b3a1774ef77059463fe2a8bf14a4e9845f072
arenisca <vseron2000@gmail.com>
2026-05-20 15:08:38 -0400

    Feat: clase Invitacion y métodos según enunciado

---
+++ src/main/java/org/example/Invitacion.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.time.Instant;
+
+public class Invitacion {
+    private Instant hora;
+    private Invitable destinatario;
+
+    private Reunion reunion;
+
+    public Invitacion(Invitable destinatario, Reunion reunion, Instant hora) {
+        this.destinatario=destinatario;
+        this.hora=hora;
+        this.reunion=reunion;
+    }
+
+    public void invitar() {
+        destinatario.invitar(reunion);
+    }
+
+    public void setDestinatario(Invitable destinatario){
+        this.destinatario=destinatario;
+    }
+    public Invitable getDestinatario(){ return destinatario;}
+    public Reunion getReunion(){return reunion;}
+    public Instant getHora(){ return hora;}
+
+    public String toString(){
+        return "Invitación: "+ destinatario + ", a reunión " + reunion + " a la hora " + hora;
+    }
+}
